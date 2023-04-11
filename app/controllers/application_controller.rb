62ac64a3286b298bb0bcec96a268ee5d0b7bff88
Nayla Castillo <naylac14@gmail.com>
2023-04-11 17:12:20 +0000

    rcav - added a routing folder for /rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    
+    #render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    render({ :template =>"game_templates/user_rock.html.erb"})
+  end
 end
